d58c052d4cdfaddb06b42de7037bb41cba435a8a
joaquinnn6 <joaqu.contrerasb@duocuc.cl>
2026-09-08 23:56:25 -0300

    Add backend players

---
+++ blackout-back-sett/src/test/java/cl/duocuc/blackoutbacksett/players/PlayersServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package cl.duocuc.blackoutbacksett.players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PlayersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Verifica que el contexto de Spring levante sin errores
+    }
+
+}
